f352b0b7bb81a2cc514d537356e51cf89034e996
Veerav Naidu <veeravn@gmail.com>
2025-03-24 16:36:41 -0500

    fix setup script to use correct experient, uri for data, and env variables

--- azure_ml_env_setup.py
+++ azure_ml_env_setup.py
@@ -11,21 +11,22 @@ ml_client = MLClient(
     workspace_name="Tech-LLM"
 )
 
-# Get dataset reference
-#dataset_input = ml_client.data.get(name="instruction_dataset", version="1")
 # Define training job
 job = command(
     code="./",
     command="python azure_ml_finetune.py --data_path ${{inputs.training_data}}",
     inputs={
-        "training_data": Input(type="uri_file", path="https://techllm4159171971.blob.core.windows.net/tech-instruction-dataset/instruction_dataset.jsonl")
+        "training_data": Input(type="uri_file", mode="ro_mount", path="azureml://subscriptions/7cc0da73-46c1-4826-a73b-d7e49a39d6c1/resourcegroups/custom-tech-llm/workspaces/Tech-LLM/datastores/workspaceartifactstore/paths/instruction_dataset.jsonl")
     },
-    environment="codellama-env@latest",  # Change if you're using a custom environment
+    environment="code-llama-env@latest",  # Change if you're using a custom environment
     compute="codegen-cluster",
     display_name="Finetune-CodeLLaMA",
-    description="LoRA fine-tuning of CodeLLaMA-13B with tech instruction dataset"
+    description="LoRA fine-tuning of CodeLLaMA-13B with tech instruction dataset",
+    environment_variables={"AZURE_SUBSCRIPTION_ID":"7cc0da73-46c1-4826-a73b-d7e49a39d6c1",
+                            "AZURE_RESOURCE_GROUP":"custom-tech-llm",
+                            "AZURE_WORKSPACE_NAME":"Tech-LLM"}
 )
 
 # Submit job
-ml_client.jobs.create_or_update(job)
+ml_client.jobs.create_or_update(job, experiment_name="codellama")
 print("Job submitted to Azure ML.")
